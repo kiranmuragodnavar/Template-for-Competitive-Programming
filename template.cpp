e2183d6373ebe0af008fabeeba227d87fed57191
kiranmuragodnavar <79253510+kiranmuragodnavar@users.noreply.github.com>
2022-01-22 02:09:51 +0530

    First CP Template

---
+++ template.cpp
@@ -0,0 +1,87 @@
+// This code is given by - kiran_abc02 aka bentonite_02
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+
+#define ll long long int
+#define ull unsigned long long
+#define ld long double
+#define god 1000000007
+
+#define gcd(a, b) __gcd((a), (b))
+#define F first
+#define S second
+#define endl '\n'
+
+#define vec vector<long long int>
+//#define map map<ll, ll>
+#define smap map<char, ll>
+#define umap unordered_map<ll, ll>
+#define sumap unordered_map<char, ll>
+
+#define input(v)      \
+    for (auto &x : v) \
+    {                 \
+        cin >> x;     \
+    }
+#define repn(n) for (ll i = 0; i < n; i++)
+#define rep(a, b) for (ll i = a; i < b; i++)
+#define rep_rev(a, b) for (ll i = a; i >= b; i--)
+#define access(v) for (auto &x : v)
+#define display(v)        \
+    for (auto x : v)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define pr pair<ll, ll>
+#define sort(v) sort(v.begin(), v.end())
+#define reverse_sort(v) sort(v.begin(), v.end(), greater<int>())
+
+#define maxim(v) *max_element(v.begin(), v.end())
+#define minim(v) *min_element(v.begin(), v.end())
+#define sumof(v) accumulate(v.begin(), v.end(), 0)
+#define count(v, n) count(v.begin(), v.end())
+#define reverse(v) reverse(v.begin(), v.end())
+
+#define lcm(a, b) a / gcd(a, b) * b
+#define square(x) x *x
+#define cube(x) x *x *x
+#define cBits(x) __builtin_popcount(x)
+
+
+using namespace std;
+
+// Code begins here-->
+
+void solve()
+{ }
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll _ = 1;
+    cin >> _;
+    while (_--)
+    {
+        solve();
+    }
+
+    return 0;
+}
