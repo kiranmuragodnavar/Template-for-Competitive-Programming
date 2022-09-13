0737b760907cce10be041e0996ffe2808a8513b8
kiranmuragodnavar <79253510+kiranmuragodnavar@users.noreply.github.com>
2022-09-13 20:02:21 +0530

    Create template.cpp

--- template.cpp
+++ template.cpp
@@ -1,5 +1,7 @@
 // This code is given by - bentonite_02
 
+#pragma GCC optimize("O3,unroll-loops")
+
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -19,10 +21,20 @@
 #include <set>
 #include <vector>
 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
 #define ll long long int
 #define ull unsigned long long
 #define ld long double
 #define god 1000000007
+#define god1 998244353
+#define INF 1e18
+#define pi 3.141592653589793238462
+#define siz(x) ((ll)(x).size())
+#define all(x) (x).begin(), (x).end()
 
 #define gcd(a, b) __gcd((a), (b))
 #define F first
@@ -31,7 +43,7 @@
 
 #define vec vector<long long int>
 #define ap map<ll, ll>
-#define sap map<string, ll>
+#define sap map<char, ll>
 #define uap unordered_map<ll, ll>
 #define suap unordered_map<string, ll>
 
@@ -51,11 +63,15 @@
         cout << x << " "; \
     }                     \
     cout << endl;
-#define pair pair<ll, ll>
+#define pr pair<ll, ll>
 #define debug(x) cout << #x << " " << x << endl;
 #define sort(v) sort(v.begin(), v.end())                         // Sort a container
 #define reverse_sort(v) sort(v.begin(), v.end(), greater<int>()) // Reverse container in sorted order
-
+#define sumv(v, a)   \
+    for (auto x : v) \
+    {                \
+        a += x;      \
+    }
 #define maxim(v) *max_element(v.begin(), v.end())
 #define minim(v) *min_element(v.begin(), v.end())
 #define suof(v) accumulate(v.begin(), v.end(), 0) // Calculate sum of elements in a container
@@ -69,8 +85,54 @@
 #define cube(x) x *x *x
 #define setbits(x) __builtin_popcount(x) // Count Set Bits
 
-using namespace std;
+ll expo(ll a, ll b, ll mod)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+ll mod_add(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll modFact(ll n, ll p)
+{
+    if (n >= p)
+        return 0;
+    ll result = 1;
+    for (ll i = 1; i <= n; i++)
+    {
+        result = (result * i) % p;
+    }
+    return result;
+}
+bool valid(ll x, ll y, ll n, ll m) { return (x >= 0 && x <= n - 1 && y >= 0 && y <= m - 1); }
+ll dist(ll i, ll j, ll sx, ll sy)
+{
+    return abs(sx - i) + abs(sy - j);
+}
 
+using namespace std;
 // Code begins here-->
 
 void solve()
@@ -79,8 +141,8 @@ void solve()
 
 int main()
 {
-    // freopen("input.txt","r",stdin);
-    // freopen("output.txt","w",stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -88,7 +150,7 @@ int main()
     cin >> _;
     while (_--)
     {
-        // cout << "Case #" << p << ": "; // **Used for Google competitions only**
+        //  cout << "Case #" << p << ": "; // **Used for Google competitions only**
         solve();
         p++;
     }
