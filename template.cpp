05443e7b3795693d42983ef54dc34bf223317638
Kiran S Muragodnavar <79253510+kiranmuragodnavar@users.noreply.github.com>
2023-03-11 12:02:38 +0530

    Updated segment trees template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 // This code is given by - bentonite_02
-
+ 
 #pragma GCC optimize("O3,unroll-loops")
-
+ 
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -20,12 +20,12 @@
 #include <random>
 #include <set>
 #include <vector>
-
+ 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
+ 
 using namespace __gnu_pbds;
-
+ 
 #define ll long long int
 #define ull unsigned long long
 #define ld long double
@@ -35,20 +35,18 @@ using namespace __gnu_pbds;
 #define pi 3.141592653589793238462
 #define siz(x) ((ll)(x).size())
 #define all(x) (x).begin(), (x).end()
-
+ 
 #define gcd(a, b) __gcd((a), (b))
 #define F first
 #define S second
 #define endl '\n'
-
-#define vec vector<long long int>
+ 
+#define vec vector<ll>
 #define ap map<ll, ll>
-#define uap map<ll, ll>
-#define cap map<char, ll>
-#define cuap unordered_map<char, ll>
-#define sap map<string, ll>
+#define sap map<char, ll>
+#define uap unordered_map<ll, ll>
 #define suap unordered_map<string, ll>
-
+ 
 #define input(v)      \
     for (auto &x : v) \
     {                 \
@@ -76,18 +74,69 @@ using namespace __gnu_pbds;
     }
 #define maxim(v) *max_element(v.begin(), v.end())
 #define minim(v) *min_element(v.begin(), v.end())
-#define suof(v) accumulate(v.begin(), v.end(), 0) // Calculate sum of elements in a container
-#define count(v, n) count(v.begin(), v.end())     // Count no of elements of a container
-#define reverse(v) reverse(v.begin(), v.end())    // Reverse a container
+#define count(v, n) count(v.begin(), v.end())  // Count no of elements of a container
+#define reverse(v) reverse(v.begin(), v.end()) // Reverse a container
 #define ax(a, b) max(a, b)
 #define in(a, b) min(a, b)
-
+ 
 #define lc(a, b) a / gcd(a, b) * b
 #define square(x) x *x
 #define cube(x) x *x *x
 #define setbits(x) __builtin_popcount(x) // Count Set Bits
-
-ll expo(ll a, ll b, ll mod)
+ 
+using namespace std;
+ 
+vec seg(1e6 + 7);
+ 
+void build(ll node, ll st, ll end, vec &v)
+{
+    if (st == end)
+    {
+        seg[node] = v[st];
+        return;
+    }
+    ll mid = (st + end) / 2;
+    build(2 * node, st, mid, v);
+    build(2 * node + 1, mid + 1, end, v);
+    seg[node] = seg[2 * node] + seg[2 * node + 1];
+}
+ 
+void update(ll node, ll st, ll end, vec &v, ll i, ll val)
+{
+    if (st == end)
+    {
+        v[st] = val;
+        seg[node] = val;
+        return;
+    }
+    ll mid = (st + end) / 2;
+    if (i <= mid)
+    {
+        update(2 * node, st, mid, v, i, val);
+    }
+    else
+    {
+        update(2 * node + 1, mid + 1, end, v, i, val);
+    }
+    seg[node] = seg[2 * node] + seg[2 * node + 1];
+}
+ll query(ll node, ll st, ll end, vec &v, ll p, ll q)
+{
+    if (st > q || p > end)
+    {
+        return 0;
+    }
+    if (p <= st && q >= end)
+    {
+        return seg[node];
+    }
+    ll mid = (st + end) / 2;
+    ll q1 = query(2 * node, st, mid, v, p, q);
+    ll q2 = query(2 * node + 1, 1 + mid, end, v, p, q);
+    return q1 + q2;
+}
+ 
+ll binary_expo(ll a, ll b, ll mod)
 {
     ll res = 1;
     while (b > 0)
@@ -99,24 +148,28 @@ ll expo(ll a, ll b, ll mod)
     }
     return res;
 }
+ 
 ll mod_add(ll a, ll b, ll m)
 {
     a = a % m;
     b = b % m;
     return (((a + b) % m) + m) % m;
 }
+ 
 ll mod_mul(ll a, ll b, ll m)
 {
     a = a % m;
     b = b % m;
     return (((a * b) % m) + m) % m;
 }
+ 
 ll mod_sub(ll a, ll b, ll m)
 {
     a = a % m;
     b = b % m;
     return (((a - b) % m) + m) % m;
 }
+ 
 ll modFact(ll n, ll p)
 {
     if (n >= p)
@@ -128,35 +181,40 @@ ll modFact(ll n, ll p)
     }
     return result;
 }
+ 
 bool valid(ll x, ll y, ll n, ll m) { return (x >= 0 && x <= n - 1 && y >= 0 && y <= m - 1); }
+ 
 ll dist(ll i, ll j, ll sx, ll sy)
 {
     return abs(sx - i) + abs(sy - j);
 }
-
-using namespace std;
-
+ 
 // Code begins here-->
-
+ 
 void solve()
 {
-}
 
+}
+ 
 int main()
 {
     // freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
+ 
+    // g++ filename.cpp -o a.exe
+    // .\a.exe
+ 
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     ll _ = 1, p = 1;
-    cin >> _;
+    // cin >> _;
     while (_--)
     {
-        //  cout << "Case #" << p << ": "; // **Used for Google competitions only**
+        // cout << "Case #" << p << ": "; Used for Google and Facebook competitions only
         solve();
         p++;
     }
-
+ 
     return 0;
 }
